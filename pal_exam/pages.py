081cf5d64d0d09622bfb0ec71fcec30df132bc39
koneill1994 <koneill1994@gmail.com>
2019-12-18 14:39:52 -0500

    Quality of life improvements for exam

--- pal_exam/pages.py
+++ pal_exam/pages.py
@@ -15,19 +15,6 @@ class exam_start_page(Page):
         import time
         # user has 5 minutes to complete as many pages as possible
         self.participant.vars['expiry'] = time.time() + Constants.exam_timer
-
-    
-    
-class SchoolTimeWaitPage(Page): 
-    def before_next_page(self):
-        self.player.get_pair()
-        
-    def get_timeout_seconds(self):
-        return self.participant.vars['expiry'] - time.time()
-
-    def is_displayed(self):
-        return self.get_timeout_seconds() > 3
-
         
 class exam_1(Page):
     form_model='player'
@@ -37,16 +24,20 @@ class exam_1(Page):
 
     def get_timeout_seconds(self):
         return self.participant.vars['expiry'] - time.time()
-
     def is_displayed(self):
         return self.get_timeout_seconds() > 3
+    def vars_for_template(self):
+        return dict(
+            presented_word=Constants.words[self.round_number]
+        )
+    def before_next_page(self):
+        self.player.get_pair()
+
 
 
 
 
 page_sequence = [
     exam_start_page,
-    SchoolTimeWaitPage,
     exam_1
-    
     ]
