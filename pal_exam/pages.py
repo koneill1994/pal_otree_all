9f23edaa486eb28cd0f4f55d4c15f81561e45798
koneill1994 <koneill1994@gmail.com>
2020-03-10 16:48:21 -0400

    minor fix to the exam

--- pal_exam/pages.py
+++ pal_exam/pages.py
@@ -12,9 +12,10 @@ class exam_start_page(Page):
         return self.round_number == 1
 
     def before_next_page(self):
-        import time
         # user has 5 minutes to complete as many pages as possible
         self.participant.vars['expiry'] = time.time() + Constants.exam_timer
+        self.player.get_pair()
+
         
 class exam_1(Page):
     form_model='player'
@@ -26,10 +27,6 @@ class exam_1(Page):
         return self.participant.vars['expiry'] - time.time()
     def is_displayed(self):
         return self.get_timeout_seconds() > 3
-    def vars_for_template(self):
-        return dict(
-            presented_word=Constants.words[self.round_number]
-        )
     def before_next_page(self):
         self.player.get_pair()
 
